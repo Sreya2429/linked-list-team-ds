5cb24a538c08eb8341b339f270350263d87a5318
Sreya2429 <sreyabiju2029@mca.ajce.in>
2025-09-28 23:48:00 +0530

    Update linked_list.c
    Roll 57 added main and insert functions

--- src/linked_list.c
+++ src/linked_list.c
@@ -7,7 +7,35 @@ struct Node*link;
 };
 
 
+struct Node*insertAtEnd(struct Node*head,int data){
+struct Node*newNode=createNode(data);
 
+struct Node*temp=head;
+if(temp==NULL){
+    return newNode;
+}
+while(temp->link!=NULL){
+    temp=temp->link;
+}
+temp->link=newNode;
+return head;
+};
+
+
+
+int main(){
+int myRollNo=57;
+int teammate1RollNo=50;
+struct Node*head=NULL;
+head=createNode(myRollNo);
+head->link=NULL;
+
+printf("Roll Numbers in Linked List: ");
+head=insertAtEnd(head,teammate1RollNo);
+
+print(head);
+return 0;
+}
 
 
 
